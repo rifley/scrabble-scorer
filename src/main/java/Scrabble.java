2a4a39508a75612987b64ef5ceb780c15e83cf29
Blake Womack and Riley Watts <blake@>
2017-03-15 16:31:56 -0700

    add alphabet array and corresponding value integer array

---
+++ src/main/java/Scrabble.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Scrabble {
+
+  String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+  int[] letterPoints = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+
+  public Integer calculateScore(String word){
+
+    System.out.println("word" + word);
+    Integer result = 0;
+    // List<Object> res = new ArrayList<Object>();
+
+    word.toLowerCase();
+    String[] wordLetters = word.split("");
+
+    System.out.println("wordletters" + wordLetters.length);
+
+    for(int i = 0; i<wordLetters.length;i++){
+      if((wordLetters[i].equals("a")) || (wordLetters[i].equals("e"))){
+        result+=1;
+      }
+      else {
+        System.out.println("else");
+      }
+
+    }
+    return result;
+  }
+
+}
---
+++ src/test/java/ScrabbleTest.java
@@ -0,0 +1,22 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ScrabbleTest {
+
+  @Test
+  public void calculateScore_returnsScoreForSingleLetter_1() {
+    Scrabble testScrabble = new Scrabble();
+    Integer expected = 1;
+    assertEquals(expected, testScrabble.calculateScore("a"));
+  }
+  @Test
+  public void calculateScore_returnsScoreForTwoLetters_1() {
+    Scrabble testScrabble = new Scrabble();
+    Integer expected = 2;
+    assertEquals(expected, testScrabble.calculateScore("ae"));
+  }
+
+
+
+
+}
